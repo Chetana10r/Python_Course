41f83ffbfd32ada72305c6d7d6cbd942ccfeca62
Chetana Rane <160422912+Chetana10r@users.noreply.github.com>
2025-02-16 23:40:39 +0530

    Adding the Files

---
+++ Modulus/test.py
@@ -0,0 +1,8 @@
+from Package.maths import *
+from Package.subpackages.mult import multiply 
+
+print(addition(2,3))
+print(substraction(3,2))
+print(multiply(4,5))
+
+#output cd modulus...python test.py
